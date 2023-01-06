9d702c714f9216dfc4f6c0500f31a0092b907094
Rudyard1 <115188735+Rudyard1@users.noreply.github.com>
2023-01-06 15:40:25 +0000

    added FPS counter for debugging
    FPS counter added

--- gesture_detection.py
+++ gesture_detection.py
@@ -3,10 +3,11 @@
 ### Date: 02/01/2023 
 ### Name: gesture_detection
 ### Description: gesture detection class using MediaPipe Pose model with PyTorch framework
-### including full annotations. Working in the presence of occlusion and lack of human in frame.
+### including full annotations. Working in the presence of occlusion and lack of human in frame. including FPS counter.
 ######################################
 import mediapipe as mp
 import cv2 
+import time 
 
 import pose_classifier
 import landmarks
@@ -31,7 +32,11 @@ pose =  mp_pose.Pose(min_detection_confidence=0.5,
                     model_complexity=1,
                     smooth_landmarks=True,
                     enable_segmentation=False,
-                    smooth_segmentation=True,)        
+                    smooth_segmentation=True,)  
+
+# initialise fps variables.
+t0 = 0
+t1 = 0      
 
 # begin video processing.
 while cap.isOpened():
@@ -47,18 +52,30 @@ while cap.isOpened():
   # pass image to models.
   image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
   pose_results = pose.process(image)
+
+  # FPS calculation.
+  t1 = time.time()
+  fps = 1/(t1 - t0)
+  t0 = t1
+  fps = int(fps)
+  fps = str(fps)
   
-  # Draw the pose annotation on the image.
+  # Draw the pose and FPS annotation on the image.
   image.flags.writeable = True
   image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+  font = cv2.FONT_HERSHEY_SIMPLEX
+  
   mp_drawing.draw_landmarks(
       image,
       pose_results.pose_landmarks,
       mp_pose.POSE_CONNECTIONS,
       landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
-    
-  # Flip the image horizontally for a selfie-view display.
-  cv2.imshow('MediaPipe Pose', cv2.flip(image, 1))
+
+  
+  # dev tool window plus fps show.
+  cv2.putText(image, fps, (7, 70), font, 3, (100, 0, 100), 3, cv2.LINE_AA)
+  
+  cv2.imshow('MediaPipe Pose', image)
   if cv2.waitKey(5) & 0xFF == 27:
     break
 
@@ -80,6 +97,10 @@ while cap.isOpened():
  
 cap.release()
 cv2.destroyAllWindows()
+
+
+
+
 
 
 
