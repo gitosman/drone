fd69e8d90a8f84fe4a953403ff00d6171342f508
dilanp03 <92756436+dilanp03@users.noreply.github.com>
2023-05-01 10:45:07 +0100

    Update horizon.py

--- horizon.py
+++ horizon.py
@@ -9,19 +9,19 @@ def horizon(NOSE, r, l):
     #print(NOSE)
     if abs(NOSE) < 1:
         if NOSE > r:
-            print("RIGHT")
+            #print("RIGHT")
             #print(DEG)
             LEFT = False
             RIGHT = True
             IN = False
         if NOSE < l:            
-            print("LEFT")
+            #print("LEFT")
             #print(DEG)
             RIGHT = False
             LEFT = True
             IN = False
         if (RIGHT == False) and (LEFT == False):
-            print("IN")
+            #print("IN")
             IN = True
 
 
@@ -30,3 +30,20 @@ def horizon(NOSE, r, l):
         print("SEARCH") #will be search mode -> continue yaw until find nose
 
     return LEFT, RIGHT, SEARCH, DEG, IN  
+
+# def shoulder(LSDR, RSDR, r, l):
+#     diff = LSDR - RSDR
+#     frame_diff = r - l
+#     user_diff = frame_diff - diff
+#     if user_diff < 0:
+#         print("Too close - move back")
+#     else:
+#         print("In frame")
+    
+#     return
+
+def head_to_waist(NOSE_Y, LHIP_X, RHIP_X, LHIP_Y, RHIP_Y):
+    HIP_MIDPOINT_Y = (LHIP_Y + RHIP_Y)/2
+    HIP_MIDPOINT_X = (LHIP_X + RHIP_X)/2    
+    user_diff = HIP_MIDPOINT_Y - NOSE_Y
+    print(user_diff)
